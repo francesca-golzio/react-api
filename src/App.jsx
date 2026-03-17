406e40bbcde7a814b9a4f212d656f88ebf61c7d2
Francesca Golzio <francescagolzio@gmail.com>
2026-03-17 12:49:09 +0100

    print on console actor and actresses lists

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 
 
 function App() {
+
+  const attriciApi = 'https://lanciweb.github.io/demo/api/actresses/';
+  const attoriApi = 'https://lanciweb.github.io/demo/api/actors/';
+
+  fetch(attriciApi)
+  .then(res => res.json())
+  .then(attrici => {
+    console.log(attrici);
+    
+  })
+  .catch(error => {
+    console.error(error);
+  })
   
+  fetch(attoriApi)
+  .then(res => res.json())
+  .then(attori => {
+    console.log(attori);
+    
+  })
+  .catch(error => {
+    console.error(error);
+  })
 
   return (
     <>
